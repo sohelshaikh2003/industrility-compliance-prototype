1c314f10c95fd55e363477e735ea1b418a507cca
sohelshaikh <sohelk.252003@gmail.com>
2026-02-11 01:09:19 +0530

    updated code

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -18,6 +18,42 @@ resource "aws_s3_bucket" "evidence_bucket" {
 }
 
 # -----------------------------
+# S3 bucket policy for CloudTrail
+# -----------------------------
+resource "aws_s3_bucket_policy" "cloudtrail_policy" {
+  bucket = aws_s3_bucket.evidence_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.evidence_bucket.arn
+      },
+      {
+        Sid    = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.evidence_bucket.arn}/AWSLogs/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# -----------------------------
 # CloudTrail (SOC 2 Logging Control)
 # -----------------------------
 resource "aws_cloudtrail" "audit_trail" {
@@ -25,4 +61,8 @@ resource "aws_cloudtrail" "audit_trail" {
   s3_bucket_name                = aws_s3_bucket.evidence_bucket.id
   include_global_service_events = true
   is_multi_region_trail         = true
+
+  depends_on = [
+    aws_s3_bucket_policy.cloudtrail_policy
+  ]
 }
